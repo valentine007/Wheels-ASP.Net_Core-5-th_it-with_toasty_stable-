1eacf659c768d9d9de7f147ab1717f916464009f
Valentyn Demchuk <vdemchuk.vd@gmail.com>
2018-09-12 13:48:10 +0300

    Implement client-side filtering.

--- Wheels/ClientApp/app/components/vehicle-module/vehicle-list/vehicle-list.ts
+++ Wheels/ClientApp/app/components/vehicle-module/vehicle-list/vehicle-list.ts
@@ -6,6 +6,7 @@ import { VehicleService } from '../../../services/vehicle.service';
 })
 export class VehicleListComponent implements OnInit {
 	vehicles: Vehicle[];
+	allVehicles: Vehicle[];
 	makes: KeyValuePair[];
 	filter: any = {};
 	constructor(private vehicleService: VehicleService) { }
@@ -14,9 +15,19 @@ export class VehicleListComponent implements OnInit {
 			.subscribe(makes => this.makes = makes);
 
 		this.vehicleService.getVehicles()
-			.subscribe(vehicles => this.vehicles = vehicles);
+			.subscribe(vehicles => this.vehicles = this.allVehicles = vehicles);
 	}
 
 	onFilterChange() {
+		var vehicles = this.allVehicles;
+		if (this.filter.makeId)
+			vehicles = vehicles.filter(v => v.make.id == this.filter.makeId);
+		if (this.filter.modelId)
+			vehicles = vehicles.filter(v => v.model.id == this.filter.modelId);
+		this.vehicles = vehicles;
+	}
+	resetFilter() {
+		this.filter = {};
+		this.onFilterChange();
 	}
 }
