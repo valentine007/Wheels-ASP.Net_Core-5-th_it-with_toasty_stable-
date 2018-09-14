0b485c8316ef240b31dd58e31016eaf9970630fb
Valentyn Demchuk <vdemchuk.vd@gmail.com>
2018-09-14 12:23:04 +0300

    Fix the memory leak

--- Wheels/ClientApp/app/services/progress.service.ts
+++ Wheels/ClientApp/app/services/progress.service.ts
@@ -1,26 +1,44 @@
 ﻿import { Injectable } from '@angular/core';
 import { Subject } from "rxjs/Subject";
 import { BrowserXhr } from "@angular/http";
+
 @Injectable()
 export class ProgressService {
-	uploadProgress: Subject<any> = new Subject();
-	downloadProgress: Subject<any> = new Subject();
+	private uploadProgress: Subject<any>;
+
+	startTracking() {
+		this.uploadProgress = new Subject();
+		return this.uploadProgress;
+	}
+
+	notify(progress) {
+		this.uploadProgress.next(progress);
+	}
+
+	endTracking() {
+		this.uploadProgress.complete();
+	}
 }
+
 @Injectable()
 export class BrowserXhrWithProgress extends BrowserXhr {
+
 	constructor(private service: ProgressService) { super(); }
+
 	build(): XMLHttpRequest {
 		var xhr: XMLHttpRequest = super.build();
 
-		xhr.onprogress = (event) => {
-			this.service.downloadProgress.next(this.createProgress(event));
-		};
 		xhr.upload.onprogress = (event) => {
-			this.service.uploadProgress.next(this.createProgress(event));
+			this.service.notify(this.createProgress(event));
 		};
+
+		xhr.upload.onloadend = () => {
+			this.service.endTracking();
+		}
 
 		return xhr;
 	}
+
 	private createProgress(event) {
 		return {
 			total: event.total,
